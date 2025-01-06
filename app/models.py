7789615a3a1b46533f6c8116d0d302b50889caa9
bachkaaaaa <bachkamohamed1@gmail.com>
2025-01-06 16:58:50 +0100

    [FIXED] Sentences in finnish added, Search works for finnish words

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from . import db
 class Word(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(100), nullable=False)
+    word_example_sentence = db.Column(db.String(255))
     translation = db.Column(db.String(100), nullable=False)
     example_sentence = db.Column(db.String(255))
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template,request,redirect,url_for
 from app.models import Word
 from app import db
-from db.load_data import load_initial_data; app = Blueprint("app", __name__)
+from sqlalchemy import or_  # Add this import
+
+from db.load_data import load_initial_data;
+app = Blueprint("app", __name__)
 @app.before_app_request
 def before_first():
     # Load initial data from the JSON file if not already loaded
@@ -11,7 +14,12 @@ def before_first():
 def index():
     search_query = request.args.get('search', '')
     if search_query:
-        words = Word.query.filter(Word.word.like(f"%{search_query}%")).all()
+        words = Word.query.filter(
+            or_(
+                Word.word.like(f"%{search_query}%"),
+                Word.translation.like(f"%{search_query}%")
+            )
+        ).all()
     else:
         words = Word.query.all()
     return render_template("index.html", words=words)
@@ -21,6 +29,7 @@ def edit_word(id):
     word = Word.query.get(id)
     if request.method == "POST":
         word.word = request.form["word"]
+        word.word_example_sentence=request.form["word_example_sentence"]
         word.translation = request.form["translation"]
         word.example_sentence = request.form["example_sentence"]
         db.session.commit()
